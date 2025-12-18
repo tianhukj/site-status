36be5c9e78b6be4cd7ceaa5b87fa6055d0303055
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-12-18 20:10:05 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -32,7 +32,7 @@
       </n-p>
       <n-p depth="3">
         Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> IMSYY </n-text>
+        <n-text depth="3" @click="jumpLink(linkData.home)"> TIANHU </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
